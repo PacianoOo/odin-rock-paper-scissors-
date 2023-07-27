40d8bd41132907996dbb929fbbfcb9c86bc9332a
pacianoseth <feintimg@gmail.com>
2023-07-27 20:35:38 +0800

    finalized solution, added button transition and image transition

--- script.js
+++ script.js
@@ -9,25 +9,36 @@ const output = document.querySelector('#output > div.round_output');
 const player_score = document.querySelector('#output > div.player_score');
 const computer_score = document.querySelector('#output >div.computer_score');
 const reload_btn = document.querySelector('.middle > button.hidden');
+const p_img_transition_rock = document.querySelector('.middle > .player_side > .images > img.player_rock');
+const p_img_transition_paper = document.querySelector('.middle > .player_side > .images > img.player_paper');
+const p_img_transition_scissors = document.querySelector('.middle > .player_side > .images > img.player_scissors');
 
+const c_img_transition_rock = document.querySelector('.middle > .computer_side > .images > img.computer_rock');
+const c_img_transition_paper = document.querySelector('.middle > .computer_side > .images > img.computer_paper');
+const c_img_transition_scissors = document.querySelector('.middle > .computer_side > .images > img.computer_scissors');
 game();
 function choice(){
     if (this.id === 'rock') {
-     input = 1;
-        
+    input = 1;
+    p_img_transition_rock.classList.add('hover');
+    p_img_transition_paper.classList.remove('hover');
+    p_img_transition_scissors.classList.remove('hover');
     }
     else if (this.id === 'paper'){
      input = 2;
-        
+     p_img_transition_rock.classList.remove('hover');
+     p_img_transition_paper.classList.add('hover');
+     p_img_transition_scissors.classList.remove('hover');
     }
     else if (this.id === 'scissors'){
      input = 3;
-
+     p_img_transition_rock.classList.remove('hover');
+     p_img_transition_paper.classList.remove('hover');
+     p_img_transition_scissors.classList.add('hover');
     }
     else {
     console.log('another button pressed!');
     }
-
     computer_selection();
     compare();
 }
@@ -39,11 +50,28 @@ function game(){
     btns.forEach(btn => btn.addEventListener('click', choice));
     computer_score.textContent = 'Computer Score: ' + computerScore;
     player_score.textContent = 'Player Score: ' + playerScore;
+    
 
+    
 }
 
 function computer_selection(){
     randomSelection = Math.floor(Math.random() * 3) + 1;
+    if (randomSelection === 1){
+        c_img_transition_rock.classList.add('hover');
+        c_img_transition_paper.classList.remove('hover');
+        c_img_transition_scissors.classList.remove('hover');
+    }
+    else if (randomSelection === 2){
+        c_img_transition_rock.classList.remove('hover');
+        c_img_transition_paper.classList.add('hover');
+        c_img_transition_scissors.classList.remove('hover');
+    }
+    else if (randomSelection === 3){
+        c_img_transition_rock.classList.remove('hover');
+        c_img_transition_paper.classList.remove('hover');
+        c_img_transition_scissors.classList.add('hover');
+    }
     return randomSelection;
 }
 
@@ -52,6 +80,7 @@ function compare(){
     if (input === randomSelection){
         console.log('tie');
         output.textContent= 'TIE!';
+        
     }
     else if ((input == 1 && randomSelection == 2) || (input == 2 && randomSelection == 3) || (input == 3 && randomSelection == 1)){
         console.log('computer win');
